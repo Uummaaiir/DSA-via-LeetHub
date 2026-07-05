ddfb643d70aa0a9c0e6394340697204abf3576e3
Uummaaiir <161137470+Uummaaiir@users.noreply.github.com>
2026-07-05 21:05:49 +0530

    Time: 1 ms (99.17%), Space: 42.5 MB (73.28%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -14,3 +14,27 @@ class Solution {
 
     }
 }
+
+// class Solution {
+//     public int score(int n) {
+
+//         // Array to store the frequency of digits 0 to 9
+//         int[] freq = new int[10];
+
+//         // Count the frequency of each digit
+//         while (n > 0) {
+//             int digit = n % 10;   // Get the last digit
+//             freq[digit]++;        // Increase the frequency of that digit
+//             n = n / 10;           // Remove the last digit
+//         }
+
+//         // Calculate the score
+//         int score = 0;
+
+//         for (int i = 0; i < 10; i++) {
+//             score = score + (i * freq[i]);
+//         }
+
+//         return score;
+//     }
+// }
