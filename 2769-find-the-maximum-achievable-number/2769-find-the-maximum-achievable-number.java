5080b014457faf02b00ad0fb196d74ac21f6da9b
Uummaaiir <161137470+Uummaaiir@users.noreply.github.com>
2026-07-06 08:52:34 +0530

    Time: 1 ms (100%), Space: 43.1 MB (82.82%) - LeetHub

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+
+    //     int x=num;
+    //     for(int i=0; i<t; i++){
+    //         x= x+2;
+    //     }
+    //     return x;
+           
+           return num+ 2*t;
+
+        //    int x= num;
+
+        //    while(t>0){
+        //     x=x+2;
+        //     t--;
+        //    }
+
+        //    return x;
+
+     }
+}
